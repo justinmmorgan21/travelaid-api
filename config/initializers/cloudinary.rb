93f8cc4d58e787e92fdac6245162ab55384f3285
justinmmorgan21 <justinmorgan21@gmail.com>
2024-11-27 23:51:09 -0600

    move cloudinary keys from credentials.yml to .env

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,7 @@
 Cloudinary.config do |config|
-  config.cloud_name = Rails.application.credentials[:cloudinary_cloud_name]
-  config.api_key = Rails.application.credentials[:cloudinary_api_key]
-  config.api_secret = Rails.application.credentials[:cloudinary_api_secret]
+  config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
+  config.api_key = ENV['CLOUDINARY_API_KEY']
+  config.api_secret = ENV['CLOUDINARY_API_SECRET']
   config.secure = true
   config.cdn_subdomain = true
 end
